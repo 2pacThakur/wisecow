8b5cb9aa542ea9c2cca74a6c9247cad78e2cd1ec
2pacThakur <89178965+2pacThakur@users.noreply.github.com>
2024-06-18 23:23:43 +0530

    Update wisecow.sh

--- wisecow.sh
+++ wisecow.sh
@@ -14,19 +14,19 @@ get_api() {
 handleRequest() {
     # 1) Process the request
 	get_api
-	mod=`fortune`
+	mod=$(/User/Chaitanya/fortune)
 
 cat <<EOF > $RSPFILE
 HTTP/1.1 200
 
 
-<pre>`cowsay $mod`</pre>
+<pre>`/User/Chaitanya/cowsay $mod`</pre>
 EOF
 }
 
 prerequisites() {
-	command -v cowsay >/dev/null 2>&1 &&
-	command -v fortune >/dev/null 2>&1 || 
+	command -v /User/Chaitanya/cowsay >/dev/null 2>&1 &&
+	command -v /User/Chaitanya/fortune >/dev/null 2>&1 || 
 		{ 
 			echo "Install prerequisites."
 			exit 1
